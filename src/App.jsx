5336a36a7130d755799122542b61c8644f78baa0
Deiu <d3iu25@gmail.com>
2023-12-06 17:28:26 +0200

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home/Home';
+import LiveProvider from './components/liveProvider/LiveProvider';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<LiveProvider />} />
       </Routes>
     </BrowserRouter>
     </>
