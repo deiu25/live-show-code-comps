2159459f097e3ea485f4721622b14c7309bfbc44
Deiu <d3iu25@gmail.com>
2023-12-12 17:21:22 +0200

    app change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home/Home';
-
+import axios from 'axios';
 import { HomeAllPosts } from './components/home/home-posts/HomeAllPosts';
 import { About } from './pages/about/About';
 import { Contact } from './pages/contact/Contact';
 import { Login } from './auth/pages/auth/Login';
 import { Signup } from './auth/pages/auth/Singup';
 
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
