efbe7738bed76a5aef2a1fff1a0b5c7db4560685
Badulescu Andrei <d3iu25@gmail.com>
2024-01-07 22:29:52 +0200

    refactoring PostMap.jsx with lazzy loading

--- frontend/src/components/home/posts-map/PostsMap.jsx
+++ frontend/src/components/home/posts-map/PostsMap.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { memo, useCallback, useEffect, useRef, useState } from "react";
 import Card from "../post-card/PostCard";
 
 // Componentă separată pentru heading
-const SectionHeading = ({ title }) => (
+const SectionHeading = memo(({ title }) => (
   <div className="section-heading">
     <h2 className="display-4">{title} <i className="fal fa-play small"></i></h2>
   </div>
-);
+));
 
 // Componentă pentru randarea listei de postări
-const PostList = ({ posts }) => (
+const PostList = memo(({ posts }) => (
   <div className="row">
     {posts.map((post) => (
       <div className="col-md-4" key={post._id}>
@@ -23,14 +23,51 @@ const PostList = ({ posts }) => (
       </div>
     ))}
   </div>
-);
+));
 
-// Componenta principală refăcută
-export const PostsMap = React.memo(({ posts, title }) => {
+// Componenta principală refăcută cu lazy loading
+const useLazyLoad = (posts, loadMoreRef) => {
+  const [visiblePosts, setVisiblePosts] = useState([]);
+
+  const loadMorePosts = useCallback(() => {
+    setVisiblePosts(prevPosts => [
+      ...prevPosts,
+      ...posts.slice(prevPosts.length, prevPosts.length + 6)
+    ]);
+  }, [posts]);
+
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        loadMorePosts();
+      }
+    }, { threshold: 1.0 });
+
+    if (loadMoreRef.current) {
+      observer.observe(loadMoreRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, [loadMorePosts, loadMoreRef]);
+
+  useEffect(() => {
+    setVisiblePosts(posts.slice(0, 6));
+  }, [posts]);
+
+  return visiblePosts;
+};
+
+export const PostsMap = memo(({ posts, title }) => {
+  const loadMoreRef = useRef(null);
+  const visiblePosts = useLazyLoad(posts, loadMoreRef);
+
   return (
     <div className="content-section">
       {title && <SectionHeading title={title} />}
-      <PostList posts={posts} />
+      <PostList posts={visiblePosts} />
+      <div ref={loadMoreRef} style={{ height: '20px', margin: '10px 0' }}>
+      </div>
     </div>
   );
 });
