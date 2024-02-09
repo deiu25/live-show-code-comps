85fa3821ab09abadb843577025e2ca0bb096250c
Deiu <d3iu25@gmail.com>
2024-02-09 16:03:36 +0200

    new blog card design

--- frontend/src/aplication/blog/components/blog-card/BlogCard.jsx
+++ frontend/src/aplication/blog/components/blog-card/BlogCard.jsx
@@ -7,37 +7,48 @@ import { useAuthAdminStatus } from "../../../customHooks/useAuthAdminStatus";
 
 export const BlogCard = ({ id, title, description, headerImage, user: postUser }) => {
   const { isAdmin, isUserLoggedIn, isUserCreator } = useAuthAdminStatus(postUser);
-
   const confirmDelete = useDeleteBlogPost();
 
   return (
-    <li className="blog-cards_item">
-      <div className="blog-card">
-        <div className="blog-card_image">
+    <div className="blog-card-body" id="blog-card-body">
+      <div className="blog-card spring-fever">
+        <div className="blog-card-image">
           <img src={headerImage} alt="headerImage" className="blog-img"></img>
         </div>
-        <div className="blog-card_content">
-          <h2 className="blog-card_title">{title}</h2>
-          <p className="blog-card_text">{description}</p>
-          <Link to={`/blog/${id}`} className="blog-card_link">
-            <button className="blog-btn blog-card_btn">Read More</button>
-          </Link>
-          {isUserLoggedIn && isUserCreator && isAdmin && (
-            <>
-              <button className="blog-btn blog-card_btn">Edit</button>
-              <button
-                onClick={(e) => {
-                  e.stopPropagation();
-                  confirmDelete(id);
-                }}
-                className="blog-btn blog-card_btn"
-              >
-                Delete
-              </button>
-            </>
-          )}
+        
+        <div className="title-content">
+          <h3><Link to={`/blog/${id}`}>{title}</Link></h3>
+          <div className="intro"> <Link to={`/blog/${id}`}>See More</Link> </div>
         </div>
+
+        <div className="card-info">
+          <div className="blog-card-description">{description}</div>
+          <Link to={`/blog/${id}`}>Read Article<span className="licon icon-arr icon-black"></span></Link>
+        </div>
+        <div className="utility-info">
+          <ul className="utility-list">
+            {isUserLoggedIn && isUserCreator && isAdmin && (
+              <>
+                <li><span className="licon icon-edit"></span><Link to={`/edit-blog/${id}`}>Edit</Link></li>
+                <li><span className="licon icon-delete"></span>
+                  <button onClick={(e) => {
+                    e.stopPropagation();
+                    confirmDelete(id);
+                  }}
+                  className="link-like-button"
+                  >Delete</button>
+                </li>
+              </>
+            )}
+            <li><span className="licon icon-like"></span><button className="link-like-button">2</button></li>
+            <li><span className="licon icon-com"></span><button className="link-like-button">12</button></li>
+            <li><span className="licon icon-dat"></span>03 jun 2017</li>
+            <li><span className="licon icon-tag"></span><button className="link-like-button">Photos</button>, <button className="link-like-button">Nice</button></li>
+          </ul>
+        </div>
+        <div className="gradient-overlay"></div>
+        <div className="color-overlay"></div>
       </div>
-    </li>
+    </div>
   );
 };
--- frontend/src/aplication/blog/pages/blog-posts/BlogPosts.jsx
+++ frontend/src/aplication/blog/pages/blog-posts/BlogPosts.jsx
@@ -1,7 +1,7 @@
 // BlogPosts.js
 import React, { useEffect } from "react";
 import "./BlogPosts.css";
-import { BlogCard } from "../../components/blog-card/BlogCard copy";
+import { BlogCard } from "../../components/blog-card/BlogCard";
 import { fetchBlogPosts } from "../../../../redux/features/blog/blogSlice";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -17,20 +17,18 @@ export const BlogPosts = () => {
   return (
     <div className="blog-main">
       <h1 className="blog-h1">Responsive Card Grid Layout</h1>
-      <ul className="blog-cards">
         {posts.map((post) => (
           <BlogCard
             key={post._id}
             id={post._id}
             user={post.user}
             title={post.title}
-            text={post.description}
+            description={post.description}
             headerImage={
             post.headerImage.length > 0 ? post.headerImage[0].url : ""
             }
           />
         ))}
-      </ul>
     </div>
   );
 };
